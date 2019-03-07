d5c2c38e2ea4e35070e0080420dd68278fa04fdb
管宜尧 <guanyiyao@yunsom.com>
2019-03-07 20:55:58 +0800

    add exclude.skc file support

--- internal/artisan.go
+++ internal/artisan.go
@@ -2,23 +2,51 @@ package internal
 
 import (
 	"fmt"
+	"github.com/mylxsw/go-toolkit/collection"
 	"github.com/mylxsw/go-toolkit/container"
 	"github.com/mylxsw/go-toolkit/log"
 	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 )
 
 var logger = log.Module("artisan")
 
 func Artisan(cc *container.Container, skeleton, dest string, data Data) error {
-	files, err := AllFilesInDirectory("./skeleton")
+	files, err := AllFilesInDirectory(skeleton)
 	if err != nil {
 		return fmt.Errorf("read .sk files from %s failed: %s", skeleton, err)
 	}
 
+	excludeFiles := make([]string, 0)
+
+	collection.MustNew(files).Filter(func(path string) bool {
+		return strings.HasSuffix(path, "/exclude.skc")
+	}).Each(func(ef string) {
+		excludeBytes, err := ioutil.ReadFile(ef)
+		if err != nil {
+			panic(err)
+		}
+
+		lines, err := data.Parse(string(excludeBytes))
+		if err != nil {
+			panic(err)
+		}
+
+		collection.MustNew(strings.Split(lines, "\n")).Filter(func(line string) bool {
+			return strings.TrimSpace(line) != ""
+		}).Each(func(line string) {
+			excludeFiles = append(excludeFiles, path.Join(path.Dir(ef), line))
+		})
+	})
+
 	parsedFiles := make(map[string]string)
 	for _, f := range files {
+		if stringHasPrefixes(path.Join(path.Dir(f), path.Base(f)), excludeFiles) || strings.HasSuffix(f, "/exclude.skc") {
+			continue
+		}
+
 		content, err := ioutil.ReadFile(f)
 		if err != nil {
 			return fmt.Errorf("read %s failed: %s", f, err)
@@ -50,4 +78,14 @@ func Artisan(cc *container.Container, skeleton, dest string, data Data) error {
 	logger.Infof("zip archive file has been saved to %s", dest)
 
 	return nil
+}
+
+func stringHasPrefixes(s string, prefixes []string) bool {
+	for _, p := range prefixes {
+		if strings.HasPrefix(s, p) {
+			return true
+		}
+	}
+
+	return false
 }
